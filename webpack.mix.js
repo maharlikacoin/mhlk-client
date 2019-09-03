a2515794a01b307c4109729f48a0d415cf2638f9
Superern <ernandrewgregorio@gmail.com>
2019-09-03 18:14:32 +0800

    working webpack mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,16 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+    .js('resources/js/app.js', 'public/js/app.js')
+    .sass('resources/sass/app.scss', 'public/css/app.css')
+    .copy('resources/images/favicon.png', 'public/images/favicon.png')
+    .copy('resources/images/maharlika-full-logo-white.png', 'public/images/logo-full-white.png')
+    .copy('resources/images/maharlika-full-logo-white.png', 'public/images/logo-full-white2x.png')
+    .copy('resources/images/maharlika-full-logo-colored.png', 'public/images/logo.png')
+    .copy('resources/images/maharlika-full-logo-colored.png', 'public/images/logo2x.png')
+    .copy('resources/images/maharlika-logo-white.png', 'public/images/maharlika-logo-white.png');
+
+if (mix.inProduction()) {
+    mix.version();
+}
