b7001ba7080cdfa40e46cacee6662d674ed2b02c
Superern <ernandrewgregorio@gmail.com>
2019-09-05 13:21:41 +0800

    Create User Sidebar Component

---
+++ resources/js/components/UserSidebar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="user-box d-none d-lg-block" style="padding:30px;">
+        <div class="user-image">
+            <img src="/images/eth-qr.png" alt="adddress">
+        </div>
+        <h6 class="user-name">{{ address | trimAddress | upperAddress }}</h6>
+        <ul class="btn-grp guttar-10px">
+            <li><a href="#" class="btn btn-grad">TRANSFER</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+	export default {
+		name: "UserSidebar",
+        props: {
+		    address: String
+        },
+        beforeCreate() {
+
+        }
+	}
+</script>
