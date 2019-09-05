42246c87f1f7ff1cc1c8d54be0a9646ef0b7bed9
Superern <ernandrewgregorio@gmail.com>
2019-09-05 13:19:36 +0800

    create AuthWallet middleware
    login for wallet addresses

---
+++ app/Http/Middleware/AuthWallet.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AuthWallet
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // if session does not exists
+        if (!session('authAddress')) {
+            return redirect()->route('test');
+        }
+        return $next($request);
+    }
+}
