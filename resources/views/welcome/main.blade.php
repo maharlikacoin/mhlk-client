6463234768e09d9eca5e906637c84f1527216938
Superern <ernandrewgregorio@gmail.com>
2019-09-06 14:29:00 +0800

    change of 'About Maharlika Coin'
    additional description about the maharlika coin

--- resources/views/welcome/main.blade.php
+++ resources/views/welcome/main.blade.php
@@ -9,13 +9,16 @@
                     <div class="col-lg-6">
                         <div class="nk-block-text">
                             <h2 class="title"><span class="font-weight-lighter">About</span> The Maharlika Coin</h2>
-                            <p>Deployed by the <strong>Formula Green Foundation (FGF),</strong>
-                                The Maharlika Coin (MHLK) was created as a mode of trade used to fully develop a
-                                “Shared Resources Economy.” FGF believes that the most critical resources for humanity,
-                                such as energy, water, food, shelter, and livelihood, should be shared through the best of
-                                human capital, starting with the development of fusion energy and the availability of basic water.
-                                Backed by global resources, MHLK will be accessible to millions of people with the ability
-                                to transact and utilize the coin on our growing ecosystem built on partnerships worldwide.
+                            <p>Deployed by the  <strong>Formula Green Foundation (FGF)</strong>, The Maharlika Coin (MHLK) was created as
+                                a mode of trade used to fully develop a “Shared Resources Economy.” FGF believes that
+                                the most critical resources for humanity, such as energy, water, food, shelter, and livelihood,
+                                should be shared through the best of human capital, starting with the development of fusion
+                                energy and the availability of basic water. Recognizing the crucial role that the health of
+                                our oceans and coral reef diversity play in the sustainability of the world's food supply,
+                                Formula Green also supports both non-profit organizations and technology startups engaged
+                                in ocean conservation technology and programs.Backed by global resources, MHLK will be accessible
+                                to millions of people with the ability to transact and utilize the coin on our growing ecosystem
+                                built on partnerships worldwide.</p>
                         </div>
                     </div>
                     <div class="col-lg-6">
