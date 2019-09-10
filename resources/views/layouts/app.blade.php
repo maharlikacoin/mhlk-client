584fdc6b34d50f5b2651c74b4cb1ba3717880815
Superern <ernandrewgregorio@gmail.com>
2019-09-10 10:22:37 +0800

    Refactor app layout for single pages

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="zxx" class="js">
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="Formula Green Foundation">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="For humanity, especially the Filipino people">
+    <!-- Fav Icon  -->
+    <link rel="shortcut icon" href="/images/favicon.png">
+    <!-- Site Title  -->
+    <title>@yield('title') - Maharlika Coin</title>
+    <!-- Bundle and Base CSS -->
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+
+</head>
+
+<body class="nk-body body-wider bg-light">
+<div class="nk-wrap">
+    @include('single-pages.header')
+
+    @yield('content')
+
+    @include('layouts.footer')
+
+</div>
+<div class="preloader"><span class="spinner spinner-round"></span></div>
+
+<!-- JavaScript -->
+<script src="{{ mix('/js/app.js') }}"></script>
+
+</body>
+</html>
---
+++ resources/views/single-pages/header.blade.php
@@ -0,0 +1,21 @@
+<header class="nk-header page-header is-transparent is-sticky is-shrink" id="header">
+    <!-- Header @s -->
+    <div class="header-main">
+        <div class="header-container container">
+            <div class="header-wrap">
+                <!-- Logo @s -->
+                <div class="header-logo logo">
+                    <a href="./" class="logo-link">
+                        <img class="logo-dark" src="images/logo.png" srcset="images/logo2x.png 2x" alt="logo">
+                        <img class="logo-light" src="images/logo-full-white.png" srcset="images/logo-full-white2x.png 2x" alt="logo">
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </div><!-- .header-main @e -->
+
+    <!-- Banner @s -->
+    @yield('banner')
+    <!-- .header-banner @e -->
+</header>
