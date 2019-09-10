3cb25f54b2ae5f48776c0e03477f3cb16c52770b
Superern <ernandrewgregorio@gmail.com>
2019-09-10 10:27:39 +0800

    Dropdown fixes for different ratios

---
+++ resources/js/components/UserDropdownDesktop.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="topbar-action d-none d-lg-block">
+        <ul class="topbar-action-list">
+            <li class="topbar-action-item topbar-action-link">
+                <a :href="etherscanBaseUrl + address" target="_blank">{{ address }}</a>
+            </li><!-- .topbar-action-item -->
+
+            <li class="dropdown topbar-action-item topbar-action-user" @clickoutside="hideDropdown">
+                <a href="#" @click="toggleDropdown"> <img class="icon" src="/images/profile.png" alt="thumb"> </a>
+                <div class="dropdown-menu dropdown-menu-right show" v-show="showDropdown">
+                    <div class="user-dropdown-head">
+                        <h6 class="user-name">
+                        </h6>
+                    </div>
+                    <div class="user-dropdown">
+                        <ul class="user-dropdown-links">
+                            <li><a :href="route"><i class="ti ti-power-off"></i>Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </li><!-- .topbar-action-item -->
+        </ul><!-- .topbar-action-list -->
+    </div>
+
+</template>
+
+<script>
+	export default {
+		name: "UserDropdownDesktop",
+        props: {
+		    route: String,
+            address: String
+        },
+        data() {
+		    return {
+		        etherscanBaseUrl: 'https://etherscan.io/address/',
+                showDropdown: false,
+            }
+        },
+        methods: {
+            hideDropdown() {
+                this.showDropdown = false;
+            },
+            toggleDropdown() {
+		        this.showDropdown = !this.showDropdown;
+		    },
+            onCopy(e) {
+                alert('You just copied: ' + e.text)
+            },
+            onError() {
+                alert('Failed to copy ' + e.text)
+            }
+        }
+	}
+</script>
+
+<style scoped>
+    .user-dropdown {
+        min-width: inherit;
+    }
+    .user-dropdown-head{
+        background: #b9932e;
+        padding: 1px;
+    }
+    .user-dropdown-head:after {
+        border-bottom-color: #b9932e;
+    }
+    .topbar-action-item .dropdown-menu {
+        min-width: 200px;
+    }
+    .dropdown-menu.dropdown-menu-right.show {
+        transform:  translate3d(-150px, 60px, 0px);
+    }
+</style>
---
+++ resources/js/components/UserDropdownMobile.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="dropdown topbar-action-item topbar-action-user">
+        <a href="#" data-toggle="dropdown" @click="toggleDropdown">
+            <img class="icon" src="/images/profile.png" alt="thumb">
+        </a>
+        <div class="dropdown-menu dropdown-menu-right show" v-show="showDropdown" x-placement="bottom-end">
+            <div class="user-dropdown">
+                <div class="user-dropdown-head">
+                    <h6 class="user-name">
+                        <a href="#">{{ address | trimAddress }}</a>
+                    </h6>
+                </div>
+                <div class="user-dropdown-balance">
+                    <h6>MHLK Balance</h6>
+                    <h1 class="tile-info color-dark" v-html="$options.filters.reduceSizeDecimal(balances.coin)"></h1>
+                </div>
+                <ul class="user-dropdown-btns btn-grp guttar-10px">
+                    <li>
+                        <b-button id="toggle-button" class="btn btn-grad" :class="{ 'disable': !transferrable}"
+                                  :disabled="!transferrable" @click="toggleModal">Transfer</b-button>
+                    </li>
+                </ul>
+                <div class="gaps-1x"></div>
+                <ul class="user-dropdown-links">
+                    <li><a :href="route"><i class="ti ti-power-off"></i>Logout</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    import { BButton } from 'bootstrap-vue'
+	export default {
+		name: "UserDropdown",
+        components: {
+		    BButton
+        },
+        props: {
+		    route: String,
+            address: String
+        },
+        computed: {
+		    transferrable() {
+
+            }
+        },
+        data() {
+		    return {
+		        showDropdown: false,
+		        balances: {
+		            coin: 0,
+                    ether: 0
+                }
+            }
+        },
+        methods: {
+		    hideDropdown() {
+		        this.showDropdown = false;
+            },
+            toggleDropdown() {
+                  this.showDropdown = !this.showDropdown;
+            },
+            connectToProvider() {
+                let nodeProvider = new Web3.providers.HttpProvider(this.provider);
+                return new Web3(nodeProvider);
+            },
+            maharlikaContract() {
+                return new this.web3.eth.Contract(this.contractAbi, this.contractAddress, {from: this.address});
+            },
+            getContractAbi() {
+                axios.get(this.contractAbiUrl)
+                    .then(response => {
+                        this.contractAbi = response.data;
+                    })
+            },
+            getCoinBalance() {
+                this.maharlikaContract().methods.balanceOf(this.address)
+                    .call().then(coinBalance => this.balances.coin = coinBalance);
+
+            },
+            async getEtherBalance() {
+                let weiBalance = await this.web3.eth.getBalance(this.address);
+                this.balances.ether = this.web3.utils.fromWei(weiBalance, 'ether');
+            },
+
+
+		    toggleModal() {
+
+            }
+        }
+	}
+</script>
+
+<style scoped>
+    .user-dropdown-head:after {
+        border-bottom-color: #c08f3e;
+    }
+    .user-dropdown-head {
+        background: #c08f3e;
+    }
+    a {
+        color:white;
+    }
+    .user-dropdown-links, .user-dropdown-btns {
+        border-top: white
+    }
+</style>
