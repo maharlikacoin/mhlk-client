4ed5e79694129736ba56d949aed3f77f4b6a915a
Superern <ernandrewgregorio@gmail.com>
2019-09-10 10:19:23 +0800

    Add images
    mhlk logo
    fgf logo
    team images

--- webpack.mix.js
+++ webpack.mix.js
@@ -24,7 +24,9 @@ mix
     .copy('resources/images/maharlika-full-logo-white.png', 'public/images/logo-full-white2x.png')
     .copy('resources/images/maharlika-full-logo-colored.png', 'public/images/logo.png')
     .copy('resources/images/maharlika-full-logo-colored.png', 'public/images/logo2x.png')
-    .copy('resources/images/maharlika-logo-white.png', 'public/images/maharlika-logo-white.png');
+    .copy('resources/images/maharlika-logo-white.png', 'public/images/maharlika-logo-white.png')
+    .copy('resources/images/fgf-logo.png', 'public/images/fgf-logo.png')
+    .copyDirectory('resources/images/team', 'public/images/team');
 
 if (mix.inProduction()) {
     mix.version();
