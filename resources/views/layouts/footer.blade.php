2f2fc39f0e053bf941ca81a8c3f50be1c3948ea8
Superern <ernandrewgregorio@gmail.com>
2019-09-10 22:09:41 +0800

    Add other community platforms

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -55,21 +55,51 @@
                                         </div>
                                     </li>
                                 </ul>
-                                <div class="contact-social">
+                                <div class="contact-social d-flex flex-column w-100 align-items-center">
                                     <h6>Be a part of our community</h6>
                                     <ul class="social-links">
-                                        <li><a href="https://www.facebook.com/maharlikacoin" target="_blank"><i
-                                                    class="fab fa-facebook-square"></i></a></li>
-                                        <li><a href="https://medium.com/@maharlikacoin" target="_blank"><i
-                                                    class="fab fa-medium"></i></a></li>
-                                        <li><a href="https://twitter.com/maharlikacoin" target="_blank"><i
-                                                    class="fab fa-twitter-square"></i></a></li>
-                                        <li><a href="https://t.me/themaharlikacoin" target="_blank"><i
-                                                    class="fab fa-telegram-plane"></i></a></li>
-                                        <li><a href="https://www.reddit.com/user/maharlikacoin" target="_blank"><i
-                                                    class="fab fa-reddit-square"></i></a></li>
-                                        <li><a href="https://github.com/maharlikacoin" target="_blank"><i
-                                                    class="fab fa-github-square"></i></a></li>
+                                        <li>
+                                            <a href="https://www.facebook.com/maharlikacoin" target="_blank">
+                                                <i class="fab fa-facebook-square"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="https://medium.com/@maharlikacoin" target="_blank">
+                                                <i class="fab fa-medium"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="https://twitter.com/maharlikacoin" target="_blank">
+                                                <i class="fab fa-twitter-square"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="https://t.me/themaharlikacoin" target="_blank">
+                                                <i class="fab fa-telegram-plane"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                    <ul class="social-links">
+                                        <li>
+                                            <a href="https://www.reddit.com/user/maharlikacoin" target="_blank">
+                                                <i class="fab fa-reddit-square"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="https://github.com/maharlikacoin" target="_blank">
+                                                <i class="fab fa-github-square"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="https://linkedin.com/company/maharlika-coin" target="_blank">
+                                                <i class="fab fa-linkedin"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="https://maharlikacoin.slack.com" target="_blank">
+                                                <i class="fab fa-slack"></i>
+                                            </a>
+                                        </li>
                                     </ul>
                                 </div>
                             </div>
--- resources/views/wallet/footer.blade.php
+++ resources/views/wallet/footer.blade.php
@@ -9,15 +9,51 @@
                     <!-- Community -->
                     <div class="col-lg-3 col-sm-12 mb-4 mb-sm-0">
                         <div class="wgs wgs-menu d-flex flex-column w-100 align-items-center">
-                            <h6 class="wgs-title">Be a part of our community</h6>
+                            <h6 class="wgs-title text-center">Be a part of our community</h6>
                             <div class="wgs-body">
                                 <ul class="social-links">
-                                    <li><a href="https://www.facebook.com/maharlikacoin" target="_blank"><i class="fab fa-facebook-square"></i></a></li>
-                                    <li><a href="https://medium.com/@maharlikacoin" target="_blank"><i class="fab fa-medium"></i></a></li>
-                                    <li><a href="https://twitter.com/maharlikacoin" target="_blank"><i class="fab fa-twitter-square"></i></a></li>
-                                    <li><a href="https://t.me/themaharlikacoin" target="_blank"><i class="fab fa-telegram-plane"></i></a></li>
-                                    <li><a href="https://www.reddit.com/user/maharlikacoin" target="_blank"><i class="fab fa-reddit-square"></i></a></li>
-                                    <li><a href="https://github.com/maharlikacoin" target="_blank"><i class="fab fa-github-square"></i></a></li>
+                                    <li>
+                                        <a href="https://www.facebook.com/maharlikacoin" target="_blank">
+                                            <i class="fab fa-facebook-square"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://medium.com/@maharlikacoin" target="_blank">
+                                            <i class="fab fa-medium"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://twitter.com/maharlikacoin" target="_blank">
+                                            <i class="fab fa-twitter-square"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://t.me/themaharlikacoin" target="_blank">
+                                            <i class="fab fa-telegram-plane"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                                <ul class="social-links">
+                                    <li>
+                                        <a href="https://www.reddit.com/user/maharlikacoin" target="_blank">
+                                            <i class="fab fa-reddit-square"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://github.com/maharlikacoin" target="_blank">
+                                            <i class="fab fa-github-square"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://linkedin.com/company/maharlika-coin" target="_blank">
+                                            <i class="fab fa-linkedin"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://maharlikacoin.slack.com" target="_blank">
+                                            <i class="fab fa-slack"></i>
+                                        </a>
+                                    </li>
                                 </ul>
                             </div>
                         </div>
