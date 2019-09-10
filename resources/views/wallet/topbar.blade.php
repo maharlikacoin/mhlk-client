61f68c77c930902ecacf1336cc2862c3e43a5a40
Superern <ernandrewgregorio@gmail.com>
2019-09-10 10:25:56 +0800

    Fix To show transfer button on non-desktop versions

--- resources/views/wallet/topbar.blade.php
+++ resources/views/wallet/topbar.blade.php
@@ -1,58 +1,37 @@
 <div class="topbar">
+    <!-- mobile -->
     <div class="topbar-md d-lg-none">
         <div class="container">
             <div class="d-flex align-items-center justify-content-between">
 
+                <!-- site-logo -->
                 <div class="site-logo">
                     <a href="/" class="site-brand">
-                        <img src="images/logo.png" alt="logo" srcset="images/logo2x.png 2x">
+                        <img src="images/logo.png" alt="logo" srcset="images/logo2x.png 2x" style="width:15em;">
                     </a>
-                </div><!-- .site-logo -->
-
-                <div class="dropdown topbar-action-item topbar-action-user">
-                    <a href="#" data-toggle="dropdown">
-                        <img class="icon" src="/images/profile.png" alt="thumb">
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right">
-                        <div class="user-dropdown">
-                            <ul class="user-dropdown-links">
-                                <li><a href="{{ route('wallet.logout') }}"><i class="ti ti-power-off"></i>Logout</a></li>
-                            </ul>
-                        </div>
-                    </div>
                 </div>
+
+                <user-dropdown-mobile route="{{ route('wallet.logout') }}" address="{{ session('authAddress') }}"></user-dropdown-mobile>
+
                 <!-- .toggle-action -->
             </div><!-- .container -->
         </div><!-- .container -->
     </div><!-- .topbar-md -->
+
+    <!-- desktop -->
     <div class="container">
         <div class="d-lg-flex align-items-center justify-content-between">
+
             <div class="topbar-lg d-none d-lg-block">
                 <div class="site-logo">
                     <a href="/" class="site-brand">
-                        <img src="images/logo.png" alt="logo" srcset="images/logo2x.png 2x">
+                        <img src="images/logo.png" alt="logo" srcset="images/logo2x.png 2x" style="width:15em;">
                     </a>
                 </div><!-- .site-logo -->
             </div><!-- .topbar-lg -->
 
-            <div class="topbar-action d-none d-lg-block">
-                <ul class="topbar-action-list">
-                    <li class="topbar-action-item topbar-action-link">
-                        <a href="/"><em class="ti ti-home"></em> Go to main site</a>
-                    </li><!-- .topbar-action-item -->
-
-                    <li class="dropdown topbar-action-item topbar-action-user">
-                        <a href="#" data-toggle="dropdown"> <img class="icon" src="/images/profile.png" alt="thumb"> </a>
-                        <div class="dropdown-menu dropdown-menu-right">
-                            <div class="user-dropdown">
-                                <ul class="user-dropdown-links">
-                                    <li><a href="{{ route('wallet.logout') }}"><i class="ti ti-power-off"></i>Logout</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </li><!-- .topbar-action-item -->
-                </ul><!-- .topbar-action-list -->
-            </div><!-- .topbar-action -->
+            <!-- topbar-action -->
+            <user-dropdown-desktop route="{{ route('wallet.logout') }}" address="{{ session('authAddress') }}"></user-dropdown-desktop>
         </div><!-- .d-flex -->
     </div><!-- .container -->
 </div>
