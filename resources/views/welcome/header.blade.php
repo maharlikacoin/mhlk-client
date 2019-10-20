6f0411889f92cef770d4c078e1148d09e84944c2
Superern <ernandrewgregorio@gmail.com>
2019-10-21 00:40:34 +0800

    optimize, convert possible convertible images to css

--- resources/views/welcome/header.blade.php
+++ resources/views/welcome/header.blade.php
@@ -1,64 +1,61 @@
-<header class="nk-header page-header is-transparent is-sticky is-shrink" id="header">
+<header class="nk-header page-header is-transparent is-sticky is-shrink relative overflow-hidden" id="header">
     <!-- Banner @s -->
-    <div class="header-banner bg-theme-grad">
-        <div class="nk-banner">
-            <div class="banner banner-fs banner-single banner-gap-b2">
-                <div class="banner-wrap">
-                    <div class="container">
-                        <div class="row align-items-center justify-content-center">
-                            <div class="col-lg-4 order-lg-last">
-                                <div class="banner-gfx banner-gfx-re-s1">
-                                    <div class="row align-items-center justify-content-center">
-                                        <img class="logo"
-                                             src="/images/maharlika-logo-white.png"
-                                             alt="Maharlika Coin">
-                                    </div>
+    <div class="nk-banner">
+        <div class="banner banner-fs banner-single banner-gap-b2 header-polka-mask">
+            <div class="banner-wrap">
+                <div class="container">
+                    <div class="row align-items-center justify-content-center">
+                        <div class="col-lg-4 order-lg-last">
+                            <div class="banner-gfx banner-gfx-re-s1">
+                                <div class="row align-items-center justify-content-center">
+                                    <img class="logo"
+                                         src="/images/maharlika-logo-white.png"
+                                         alt="Maharlika Coin">
                                 </div>
-                            </div><!-- .col -->
-                            <div class="col-lg-8 col-sm-10 text-center text-lg-left">
-                                <div class="banner-caption cpn tc-light">
-                                    <div class="cpn-head">
-                                        <h1 class="title">
-                                            The World's First <br>
-                                            <strong>Resource-Based Economy Cryptocurrency</strong><br>
-                                            Advocating A Dividend System <br>For Humanity
-                                        </h1>
-                                    </div>
-                                    <div class="cpn-text">
-                                        <p>The <span class="font-weight-bolder">Maharlika Coin (MHLK)</span> was created
-                                            to distribute the value of natural
-                                            resources directly to the people and selected organizations to ensure that
-                                            Humanity has a stake in the protection and conservation of our planet. We
-                                            invite everyone to use the MHLK to advocate best practices for our natural
-                                            resources and ensure world peace.
-                                        </p>
-                                    </div>
-                                    <div class="cpn-action">
-                                        @if(!session('authAddress'))
-                                            <div class="cpn-btns">
-                                                <a class="btn btn-grad" href="#" data-toggle="modal"
-                                                   data-target="#login-popup">Access your Wallet</a>
-                                            </div>
-                                        @else
-                                            <div class="cpn-btns">
-                                                <a class="btn btn-grad" href="/wallet">Go back to your Wallet</a>
-                                            </div>
-                                        @endif
-                                        <ul class="cpn-links">
-                                            <create-wallet></create-wallet>
-                                        </ul>
-                                    </div>
+                            </div>
+                        </div><!-- .col -->
+                        <div class="col-lg-8 col-sm-10 text-center text-lg-left">
+                            <div class="banner-caption cpn tc-light">
+                                <div class="cpn-head">
+                                    <h1 class="title">
+                                        The World's First <br>
+                                        <strong>Resource-Based Economy Cryptocurrency</strong><br>
+                                        Advocating A Dividend System <br>For Humanity
+                                    </h1>
                                 </div>
-                            </div><!-- .col -->
-                        </div><!-- .row -->
-                    </div>
+                                <div class="cpn-text">
+                                    <p>The <span class="font-weight-bolder">Maharlika Coin (MHLK)</span> was created
+                                        to distribute the value of natural
+                                        resources directly to the people and selected organizations to ensure that
+                                        Humanity has a stake in the protection and conservation of our planet. We
+                                        invite everyone to use the MHLK to advocate best practices for our natural
+                                        resources and ensure world peace.
+                                    </p>
+                                </div>
+                                <div class="cpn-action">
+                                    @if(!session('authAddress'))
+                                        <div class="cpn-btns">
+                                            <a class="btn btn-grad" href="#" data-toggle="modal"
+                                               data-target="#login-popup">Access your Wallet</a>
+                                        </div>
+                                    @else
+                                        <div class="cpn-btns">
+                                            <a class="btn btn-grad" href="/wallet">Go back to your Wallet</a>
+                                        </div>
+                                    @endif
+                                    <ul class="cpn-links">
+                                        <create-wallet></create-wallet>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div><!-- .col -->
+                    </div><!-- .row -->
                 </div>
             </div>
-        </div><!-- .nk-banner -->
-        <div class="nk-ovm mask-a shape-a"></div>
-
-        <!-- Place Particle Js -->
-        <div id="particles-bg" class="particles-container particles-bg"></div>
-    </div>
+        </div>
+    </div><!-- .nk-banner -->
+    <!-- Place Particle Js -->
+    <div id="particles-bg" class="particles-container particles-bg z-1"></div>
+    <div class="header-banner bg-theme-grad header-curved-mask"></div>
     <!-- .header-banner @e -->
 </header>
