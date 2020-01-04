3c7a5fd438ef536b2e47349337cb40475025e9c2
Superern <ernandrewgregorio@gmail.com>
2020-01-04 12:57:22 +0800

    remove unused prop

--- resources/js/components/wallet/main/contents/EtherTile.vue
+++ resources/js/components/wallet/main/contents/EtherTile.vue
@@ -11,9 +11,6 @@
 <script>
 	export default {
 		name: "ContentsEtherTile",
-        props: {
-            address: String
-        },
         computed: {
 		    balance() {
 		        return this.$store.getters.getEther;
