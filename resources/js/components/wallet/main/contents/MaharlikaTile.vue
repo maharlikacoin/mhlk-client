1d9599899317ebe44c1f83fe8112e9d42392161a
Superern <ernandrewgregorio@gmail.com>
2020-01-04 13:08:28 +0800

    use vuex to get coin balance

--- resources/js/components/wallet/main/contents/MaharlikaTile.vue
+++ resources/js/components/wallet/main/contents/MaharlikaTile.vue
@@ -14,35 +14,11 @@
         props: {
             address: String
         },
-        data() {
-            return {
-                apiBaseUrl: 'https://api.etherscan.io/api?',
-                module: 'account',
-                action: 'tokenbalance',
-                apikey: 'GEPXM3N11F476EMB8FCXG2XVK89Y5PKMFK',
-                contractAddress: '0xe3d0a162fcc5c02c9448274d7c58e18e1811385f',
-                balance: 0
+        computed: {
+            balance() {
+                return this.$store.getters.getCoin;
             }
-        },
-        methods: {
-		    getCoinBalance() {
-		        // etherscan does not require x-requested-with
-                delete axios.defaults.headers.common["X-Requested-With"];
-                let url = `${this.apiBaseUrl}module=${this.module}&action=${this.action}&contractaddress=${this.contractAddress}&address=${this.address}&tag=latest&apikey=${this.apikey}`;
-
-                axios.get(url)
-                    .then(response => {
-                        this.balance = response.data.result/100;
-                    })
-                    .catch(error => {
-                        console.log(error);
-                        this.balance = 0;
-                    });
-		    }
-        },
-        mounted() {
-            this.getCoinBalance();
-        },
+        }
 	}
 </script>
 
