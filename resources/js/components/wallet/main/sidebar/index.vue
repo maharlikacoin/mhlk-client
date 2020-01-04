3e7beb6e402af81eb96447811629b421782b2e5e
Superern <ernandrewgregorio@gmail.com>
2020-01-04 11:17:22 +0800

    enhance sidebar style

--- resources/js/components/wallet/main/sidebar/index.vue
+++ resources/js/components/wallet/main/sidebar/index.vue
@@ -1,10 +1,16 @@
 <template>
-    <div class="user-box d-none d-lg-block" style="padding:30px;">
+    <div class="user-box d-none d-lg-block user-panel" style="min-height: inherit;">
         <div>
             <vue-qr :text="address" logoSrc="/images/maharlika-coin-gold.png" :size="800" :logoCornerRadius="100"
                     :logoMargin="15" :margin="0"></vue-qr>
         </div>
-        <h6 class="user-name">{{ address | trimAddress }}</h6>
+        <div class="d-flex align-items-center justify-content-center">
+            <h6 class="user-name">{{ address | trimAddress }}</h6>
+            <button class="bg-transparent border-0 pointer outline-0" v-clipboard:copy="address" v-clipboard:success="onCopy"
+                    v-clipboard:error="onError">
+                <i class="fas fa-copy"></i>
+            </button>
+        </div>
         <ul class="btn-grp guttar-10px">
             <li><b-button id="toggle-button" class="btn btn-grad" :class="{ 'disable': !transferrable}"
                           :disabled="!transferrable" @click="toggleModal">Transfer</b-button>
@@ -232,6 +238,12 @@
             }
         },
         methods: {
+            onCopy(e) {
+                alert('Successfully copied: ' + e.text);
+            },
+            onError(){
+                alert('Failed to copy. Try again.');
+            },
             resetStatus() {
                 this.status = 'Status: New Transaction'
             },
@@ -408,5 +420,11 @@
 <style scoped>
     .user-image img {
         width: 130px;
+    }
+    .pointer {
+        cursor:pointer;
+    }
+    .outline-0:focus {
+        outline: 0;
     }
 </style>
