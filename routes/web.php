2089e37427403193ebca98bcb58e5434aaa227cf
Superern <ernandrewgregorio@gmail.com>
2020-01-06 17:24:10 +0800

    require authentication in server side
    teams and whitepaper

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,9 @@ Route::get('/', function () {
 
 Route::post('contact-us', 'WelcomeController@contactUs')->name('contact-us');
 
-Route::get('/whitepaper', function() {
-    return response()->file(storage_path('app/whitepaper.pdf'));
-})->name('whitepaper');
-
 Route::get('/about-us', function() {
     return view('us');
 })->name('about-us');
-
-Route::get('/team', function() {
-    return view('team');
-})->name('team');
 
 Route::get('/terms-and-conditions', function() {
     return view('terms');
@@ -45,6 +37,14 @@ Route::middleware('authWallet')->group(function() {
     Route::get('wallet/session', 'WalletController@wallet')->name('wallet.session');
     Route::get('wallet/contract', 'WalletController@contract')->name('wallet.contract');
     Route::get('wallet/logout', 'WalletController@logout')->name('wallet.logout');
+
+    Route::get('/team', function() {
+        return view('team');
+    })->name('team');
+
+    Route::get('/whitepaper', function() {
+        return response()->file(storage_path('app/whitepaper.pdf'));
+    })->name('whitepaper');
 });
 
 Route::get('/home', 'HomeController@index')->name('home');
