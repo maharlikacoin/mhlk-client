49bc620820a8b19c3983d85158972dadc7cfca79
Superern <ernandrewgregorio@gmail.com>
2020-01-26 13:25:00 +0800

    Add ether price description

--- resources/js/components/wallet/main/contents/EtherTile.vue
+++ resources/js/components/wallet/main/contents/EtherTile.vue
@@ -3,7 +3,12 @@
         <div class="tile-item tile-light">
             <div class="tile-bubbles"></div>
             <h6 class="tile-title">YOUR ETH BALANCE</h6>
-            <h1 class="tile-info color-dark">{{ $store.state.balances.ether | numberFormat('0.000000') }} ETH</h1>
+            <h1 class="tile-info color-dark">
+                {{ $store.state.balances.ether | numberFormat('0.000000') }} ETH
+                <span class="small text-black-50">
+                    (${{ $store.state.balances.ether * $store.state.prices.etherInUsd | numberFormat('0.00')}})
+                </span>
+            </h1>
         </div>
     </div>
 </template>
