4cc5d8165db5e28695f3717837c1561d13f06b72
Superern <ernandrewgregorio@gmail.com>
2020-02-18 23:29:08 +0800

    add value display

--- resources/js/components/wallet/main/contents/MaharlikaTile.vue
+++ resources/js/components/wallet/main/contents/MaharlikaTile.vue
@@ -3,7 +3,12 @@
         <div class="tile-item tile-primary">
             <div class="tile-bubbles"></div>
             <h6 class="tile-title">YOUR MHLK BALANCE</h6>
-            <h1 class="tile-info">{{ $store.state.balances.coin | numberFormat('0,000.00') }} MHLK</h1>
+            <h1 class="tile-info">
+                {{ $store.state.balances.coin | numberFormat('0,000.00') }} MHLK
+                <span class="small text-white-50">
+                    (€{{ $store.state.balances.coin | numberFormat('0,000.00') }})
+                </span>
+            </h1>
         </div>
     </div>
 </template>
