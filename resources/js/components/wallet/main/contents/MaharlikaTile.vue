dc85941690cbbb78a0b2e3d26dfe39bcfe752eda
Superern <ernandrewgregorio@gmail.com>
2020-02-22 18:26:42 +0800

    fix design and number formats

--- resources/js/components/wallet/main/contents/MaharlikaTile.vue
+++ resources/js/components/wallet/main/contents/MaharlikaTile.vue
@@ -3,19 +3,36 @@
         <div class="tile-item tile-primary">
             <div class="tile-bubbles"></div>
             <h6 class="tile-title">YOUR MHLK BALANCE</h6>
-            <h1 class="tile-info">
-                {{ $store.state.balances.coin | numberFormat('0,000.00') }} MHLK
-                <span class="small text-white-50">
-                    (€{{ $store.state.balances.coin | numberFormat('0,000.00') }})
+            <div class="tile-info relative z-1">
+                <!-- crypto -->
+                <span class="text-uppercase pointer" v-if="!showWholeCoin" @click="showWholeCoin = !showWholeCoin">
+                    MHLK {{ $store.state.balances.coin | numberFormat('0,0.[00]a') }}</span>
+                <span class="text-uppercase pointer" v-if="showWholeCoin" @click="showWholeCoin = !showWholeCoin">
+                    MHLK {{ $store.state.balances.coin | numberFormat('0,0.00') }}</span>
+
+                <!-- value -->
+                <span class="small text-white-50 text-uppercase pointer"
+                      v-if="!showWholeCoinValue" @click="showWholeCoinValue = !showWholeCoinValue">
+                    (€{{ $store.state.balances.coin | numberFormat('0,0.[00]a') }})
                 </span>
-            </h1>
+                <span class="small text-white-50 text-uppercase pointer"
+                      v-if="showWholeCoinValue" @click="showWholeCoinValue = !showWholeCoinValue">
+                    (€{{ $store.state.balances.coin | numberFormat('0,0.00') }})
+                </span>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 	export default {
-		name: "ContentsMaharlikaTile"
+		name: "ContentsMaharlikaTile",
+        data: ()=> {
+            return {
+                showWholeCoin: false,
+                showWholeCoinValue: false
+            }
+        }
 	}
 </script>
 
