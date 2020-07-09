55a1d5965138d20610325019910e234e3ef7a90d
Superern <ernandrewgregorio@gmail.com>
2020-07-10 04:49:17 +0800

    remove and rename files

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title') Project Legacy @stop
+@section('title') About Us @stop
 @section('banner')
     <div class="header-banner bg-theme-grad">
         <div class="nk-banner">
@@ -11,7 +11,7 @@
                             <div class="col-xl-6 col-lg-9">
                                 <div class="banner-caption cpn tc-light text-center">
                                     <div class="cpn-head">
-                                        <h2 class="title ttu">Project Legacy</h2>
+                                        <h2 class="title ttu">About Us</h2>
                                     </div>
                                 </div>
                             </div>
@@ -20,24 +20,8 @@
                 </div>
             </div>
         </div><!-- .nk-banner -->
-    </div>
-    <div class="nk-ovm shape-a-sm"></div>
+        <div class="nk-ovm shape-a-sm"></div>
 @stop
-
 @section('content')
-    <main class="nk-pages">
-        <section class="section section-l bg-light">
-            <div class="container">
-                <div class="nk-block nk-block-blog">
-                    <div class="row">
-                        <div class="col-12">
-                            <div id="pdf">
-                                <dossier></dossier>
-                            </div>
-                        </div><!-- .col -->
-                    </div><!-- .row -->
-                </div><!-- .nk-block -->
-            </div><!-- .container -->
-        </section><!-- .section -->
-    </main>
+    <about-us-index></about-us-index>
 @stop
