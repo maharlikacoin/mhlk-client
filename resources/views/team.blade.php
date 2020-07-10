497854e5ba42c8a57f0802db580686baa9d3e261
Superern <ernandrewgregorio@gmail.com>
2020-07-10 21:03:05 +0800

    team - update banner description

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -8,10 +8,20 @@
                 <div class="banner-wrap">
                     <div class="container">
                         <div class="row justify-content-center">
-                            <div class="col-xl-6 col-lg-9">
+                            <div class="col-md-12">
                                 <div class="banner-caption cpn tc-light text-center">
                                     <div class="cpn-head">
                                         <h2 class="title ttu">Our Team</h2>
+                                        <p class="text-justify">The MHLK Foundation is governed by its Board of Trustees
+                                            supported by its Board of Advisors to form a group of dedicated individuals
+                                            united by a common vision of a future where all of humanity has equitable
+                                            access to resources needed to lead healthy, productive lives. With its wide
+                                            range of knowledge and experience, hailing from backgrounds in business
+                                            development, product development, mergers and acquisitions, wealth and asset
+                                            management, international trade, and entrepreneurship, as well as artificial
+                                            intelligence, nanotechnology, and quantum physics, Its role is to help
+                                            support the foundation with expertise, and to help ensure the sustainable
+                                            growth of the benefit for humanity program.</p>
                                     </div>
                                 </div>
                             </div>
