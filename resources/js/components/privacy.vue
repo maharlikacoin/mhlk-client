e8e3ad0627deec4d895ef81297518cc7fe2e5734
Superern <ernandrewgregorio@gmail.com>
2020-07-10 21:03:40 +0800

    privacy - update privacy content

--- resources/js/components/privacy.vue
+++ resources/js/components/privacy.vue
@@ -10,44 +10,88 @@
 
                                     <div class="col-xl-8 col-lg-10">
                                         <ul class="blog-meta">
-                                            <li><a href="#">September 07, 2019</a></li>
+                                            <li><a href="#">Last Updated on July 10, 2020</a></li>
                                             <li><a href="#">Privacy Policy</a></li>
                                         </ul>
                                         <div class="blog-content text-justify height-2em">
                                             Maharlikacoin.com values your privacy and we do what we can to minimize the
                                             exposure of personal data during the use of our interface. Maharlikacoin.com
                                             does not collect, hold, or store keys, account information, or passwords.
-                                            We do not collect information about an individual such as the individual’s name,
-                                            telephone number, or e-mail address. We do not collect data to sell or use for
+                                            We do not collect information about an individual such as the individual’s
+                                            name,
+                                            telephone number, or e-mail address. We do not collect data to sell or use
+                                            for
                                             marketing or advertising. <br>
                                             <br>
                                             Due to the nature of blockchain, especially the Ethereum Blockchain that the
                                             Maharlika Coin is deployed, transactions of individuals who utilize
-                                            Maharlikacoin.com may be publicly accessible. This includes, but is not limited
-                                            to, your public key, the public key of the receiver, the amount sent or received,
+                                            Maharlikacoin.com may be publicly accessible. This includes, but is not
+                                            limited
+                                            to, your public key, the public key of the receiver, the amount sent or
+                                            received,
                                             and any other data a user has chosen to include in a given transaction.
                                             Information stored on a blockchain may be public, immutable, and difficult
                                             or even impossible to remove or delete. <br>
                                             <br>
-                                            Users are encouraged to review how privacy and transparency on the blockchain works.<br>
+                                            Users are encouraged to review how privacy and transparency on the
+                                            blockchain works.<br>
                                             <br>
-                                            We may collect Personal Information you choose to provide to us. For example,
+                                            We may collect and process Personal Data about your use of our website. This
+                                            data may include:
+                                            <br>
+                                            <br>
+                                            <ol class="list-group">
+                                                <li class="list-group-item">the browser types and versions used;</li>
+                                                <li class="list-group-item">the operating system used by the accessing system;</li>
+                                                <li class="list-group-item">the website from which an accessing system reaches our website
+                                                    (so-called referrers);
+                                                </li>
+                                                <li class="list-group-item">behaviour: subpage, duration, and revisit</li>
+                                                <li class="list-group-item">the date and time of access to our website,</li>
+                                                <li class="list-group-item">the Internet protocol address (“IP address”);</li>
+                                                <li class="list-group-item">the Internet service provider of the accessing system; and</li>
+                                                <li class="list-group-item">any other similar data and information that may be used in the event
+                                                    of attacks on our information technology systems.
+                                                </li>
+                                            </ol>
+                                            <br>
+                                            This data may be processed in order to deliver the content of our website
+                                            correctly, to optimize the content of our website to ensure the long-term
+                                            viability of our information technology systems and website technology, and
+                                            to provide law enforcement authorities with the information necessary for
+                                            criminal prosecution in case of a cyber-attack.
+                                            <br>
+                                            <br>
+                                            The legal basis for this processing is our legitimate business interests,
+                                            namely monitoring and improving our website and the proper protection of our
+                                            business against risks and your consent when agreeing to accept cookies
+                                            <br>
+                                            <br>
+                                            We may collect Personal Information you choose to provide to us. For
+                                            example,
                                             when you contact us for support through the Services, you give us with your
                                             e-mail address and any other information that you choose to provide. We will
                                             not disclose your Personal Information other than as described below, and we
                                             do not and will not sell your Personal Information to anyone. <br>
-                                            We may share the Personal Information we collect from and about you to fulfill
-                                            the purpose for which you provided it; with your consent; for legal, protection,
+                                            We may share the Personal Information we collect from and about you to
+                                            fulfill
+                                            the purpose for which you provided it; with your consent; for legal,
+                                            protection,
                                             and safety purposes; to comply with any court order, law, or legal process,
                                             including to respond to any government or regulatory request; to protect the
                                             rights of the Maharlikacoin.com, partners, agents, customers, and others,
-                                            including by enforcing our agreements, policies, and terms of service; and with
-                                            those vendors and suppliers we work with to provide this service. We will retain
-                                            the information you provide ONLY to process any requests and provide support.
+                                            including by enforcing our agreements, policies, and terms of service; and
+                                            with
+                                            those vendors and suppliers we work with to provide this service. We will
+                                            retain
+                                            the information you provide ONLY to process any requests and provide
+                                            support.
                                             <br>
                                             <br>
-                                            We welcome your comments or questions about this Policy. <br>
-                                            You may contact us at: <a href="mailto:support@maharlikacoin.com">support@maharlikacoin.com</a>
+                                            <p class="text-left">
+                                                We welcome your comments or questions about this Policy. <br>
+                                                You may contact us at: <a href="mailto:support@maharlikacoin.com">support@maharlikacoin.com</a>
+                                            </p>
 
                                         </div>
                                     </div>
@@ -62,9 +106,9 @@
 </template>
 
 <script>
-	export default {
-		name: "privacy"
-	}
+    export default {
+        name: "privacy"
+    }
 </script>
 
 <style scoped>
