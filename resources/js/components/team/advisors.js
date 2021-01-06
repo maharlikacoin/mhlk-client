254f1c76974eee398e9e183a31129b428487366a
Superern <ernandrewgregorio@gmail.com>
2021-01-07 01:38:09 +0800

    team - update positions

--- resources/js/components/team/advisors.js
+++ resources/js/components/team/advisors.js
@@ -7,108 +7,113 @@ const terry = 'terry.jpg';
 const denis = 'denis.jpg';
 
 const advisors = [
-  {
-      team: [
-          {
-              name: 'David Traub',
-              nickname: 'David',
-              position: '',
-              image: david,
-              nationalities: ['us'],
-              social: {
-                  linkedIn: {
-                      url: 'https://www.linkedin.com/in/david-c-traub-3542a75',
-                      icon: 'fa-linkedin-in'
-                  }
-              },
-              bio: 'David has 35 years of experience as an award-winning executive producer of feature films, digital media products and games. He\'s a philanthropic social impact activist and philanthropist and has worked on many social impact project as a producer. He\'s an investor and venture catalyst working on global business and economic development opportunities. He\'s been on the board of over 47 startup, early stage, institutional, multi-national, and government entities. He went to UC Berkeley and furthered his education.at Harvard University Graduate School of Education.'
-          },
-          {
-              name: 'Ern Gregorio',
-              nickname: 'Ern',
-              position: '',
-              image: ern,
-              nationalities: ['ph'],
-              social: {
-                  linkedIn: {
-                      url: 'https://www.linkedin.com/in/eacgregorio',
-                      icon: 'fa-linkedin-in'
-                  }
-              },
-              bio: 'Ern Gregorio is one of the youngest member of the Board and is Stardust389\'s Vice President Information Technologies who solicit and anticipate partner needs and keep up with leading practices to create our IT strategy. He also oversees all aspects of technology including digital assets, artificial intelligence, cloud infrastructure and future initiatives of the companies and the foundation.'
-          },
-          {
-              name: 'Fred Ketcher',
-              nickname: 'Fred',
-              position: '',
-              image: fred,
-              nationalities: ['us'],
-              social: {
-                  linkedIn: {
-                      url: 'https://www.linkedin.com/in/fred-ketcher-46798629',
-                      icon: 'fa-linkedin-in'
-                  }
-              },
-              bio: 'Fred Ketcher has 50 years of experience managing money for qualified investors. He has spent the last 18 years as president of FK Capital LLC. He previously held various positions on the floor of the New York Stock Exchange. Montgomery Securities as one of its youngest partners, and First Boston as vice president.'
-          },
-          {
-              name: 'Bruce Stewart',
-              nickname: 'Bruce',
-              position: '',
-              image: bruce,
-              nationalities: ['us'],
-              social: {
-                  linkedIn: {
-                      url: 'https://www.linkedin.com/in/brucemstewart',
-                      icon: 'fa-linkedin-in'
-                  }
-              },
-              bio: 'Bruce Stewart has a lifetime of multi-sector managerial achievements including sales, marketing, finance, compliance and operations. His managerial experience coupled with a history of creating and building successful brands, through identity and message development, gives me a unique vision of how these components should fit together for the whole to fully succeed.'
-          },
-          {
-              name: 'Demitri Hopkins',
-              nickname: 'Demitri',
-              position: '',
-              image: demitri,
-              nationalities: ['us'],
-              social: {
-                  linkedIn: {
-                      url: 'https://www.linkedin.com/in/demitri-hopkins-54260a106',
-                      icon: 'fa-linkedin-in'
-                  }
-              },
-              bio: 'Demitri Hopkins has been an advocate of fusion energy with a vision to power the world almost his entire life. He co-founded AGNI Energy Inc. which is developing a modular fusion reactor design combining the stability of magnetic containment with beam to target inertial fusion. He is also working with Stardust389 on its various projects on nanotechnology and fusion.'
-          },
-          {
-              name: 'Terry Mollner',
-              nickname: 'Terry',
-              position: '',
-              image: terry,
-              nationalities: ['us'],
-              social: {
-                  linkedIn: {
-                      url: 'https://www.linkedin.com/in/terry-mollner-9062326',
-                      icon: 'fa-linkedin-in'
-                  }
-              },
-              bio: 'Terry Mollner is one of the pioneers of socially responsible investing as a new asset class in the professional investment community. He is a founder and member of the board of the Calvert Family of Socially Responsible Investment Funds, the largest such family of funds with over $7 billion under management. Dr. Mollner is also the Founder and President of Trusteeship Institute, Inc., a think tank and consulting firm in economic and social development. In existence since 1973, its current focus is on the development of “common good corporations.” These are business corporations where the highest priority is the common good of humanity and nature. This is evidenced by operating as socially responsible companies and having a cap on the return to equity investors based on the risk level of the investment. All excess profit above this each year will be permanently set aside and managed forever for the common good by the company. He believes this is the next stage in the evolution of capitalism'
-          },
-          {
-              name: 'Denis Lucindo',
-              nickname: 'Denis',
-              position: '',
-              image: denis,
-              nationalities: ['ph'],
-              social: {
-                  linkedIn: {
-                      url: 'https://www.linkedin.com/in/denis-lucindo-649a117',
-                      icon: 'fa-linkedin-in'
-                  }
-              },
-              bio: 'Denis Lucindo has extensive experience in Mergers, Acquisitions, and Business Development, having spent several years as VP for Business Development for Metro Pacific Investments Corp. There, he led several and participated in several acquisitions and bids, including that for Maynilad Inc., which is the largest water distribution company with concessions covering half of Metro Manila and neighboring provinces. He has also held the role of VP for Investor Relations with Metro Pacific and First Pacific, and Chief Strategist for the Ramcar Food Group. Entrepreneurship is a key advocacy for Denis, stemming from his early days as founding Publisher and then Editor-in-Chief of the Philippine edition of Entrepreneur Magazine.'
-          },
-      ]
-  }
+    {
+        team: [
+            {
+                name: 'David Traub',
+                nickname: 'David',
+                position: '',
+                image: david,
+                nationalities: ['us'],
+                social: {
+                    linkedIn: {
+                        url: 'https://www.linkedin.com/in/david-c-traub-3542a75',
+                        icon: 'fa-linkedin-in'
+                    }
+                },
+                bio: 'David has 35 years of experience as an award-winning executive producer of feature films, digital media products and games. He\'s a philanthropic social impact activist and philanthropist and has worked on many social impact project as a producer. He\'s an investor and venture catalyst working on global business and economic development opportunities. He\'s been on the board of over 47 startup, early stage, institutional, multi-national, and government entities. He went to UC Berkeley and furthered his education.at Harvard University Graduate School of Education.'
+            },
+            {
+                name: 'Ern Gregorio',
+                nickname: 'Ern',
+                position: '',
+                image: ern,
+                nationalities: ['ph'],
+                social: {
+                    linkedIn: {
+                        url: 'https://www.linkedin.com/in/eacgregorio',
+                        icon: 'fa-linkedin-in'
+                    }
+                },
+                bio: 'Ern Gregorio is one of the youngest member of the Board and is Stardust389\'s Vice President Information Technologies who solicit and anticipate partner needs and keep up with leading practices to create our IT strategy. He also oversees all aspects of technology including digital assets, artificial intelligence, cloud infrastructure and future initiatives of the companies and the foundation.'
+            },
+            {
+                name: 'Terry Mollner',
+                nickname: 'Terry',
+                position: '',
+                image: terry,
+                nationalities: ['us'],
+                social: {
+                    linkedIn: {
+                        url: 'https://www.linkedin.com/in/terry-mollner-9062326',
+                        icon: 'fa-linkedin-in'
+                    }
+                },
+                bio: 'Terry Mollner is one of the pioneers of socially responsible investing as a new asset class in the professional investment community. He is a founder and member of the board of the Calvert Family of Socially Responsible Investment Funds, the largest such family of funds with over $7 billion under management. Dr. Mollner is also the Founder and President of Trusteeship Institute, Inc., a think tank and consulting firm in economic and social development. In existence since 1973, its current focus is on the development of “common good corporations.” These are business corporations where the highest priority is the common good of humanity and nature. This is evidenced by operating as socially responsible companies and having a cap on the return to equity investors based on the risk level of the investment. All excess profit above this each year will be permanently set aside and managed forever for the common good by the company. He believes this is the next stage in the evolution of capitalism'
+            },
+            {
+                name: 'Fred Ketcher',
+                nickname: 'Fred',
+                position: '',
+                image: fred,
+                nationalities: ['us'],
+                social: {
+                    linkedIn: {
+                        url: 'https://www.linkedin.com/in/fred-ketcher-46798629',
+                        icon: 'fa-linkedin-in'
+                    }
+                },
+                bio: 'Fred Ketcher has 50 years of experience managing money for qualified investors. He has spent the last 18 years as president of FK Capital LLC. He previously held various positions on the floor of the New York Stock Exchange. Montgomery Securities as one of its youngest partners, and First Boston as vice president.'
+            },
+            {
+                name: 'Bruce Stewart',
+                nickname: 'Bruce',
+                position: '',
+                image: bruce,
+                nationalities: ['us'],
+                social: {
+                    linkedIn: {
+                        url: 'https://www.linkedin.com/in/brucemstewart',
+                        icon: 'fa-linkedin-in'
+                    }
+                },
+                bio: 'Bruce Stewart has a lifetime of multi-sector managerial achievements including sales, marketing, finance, compliance and operations. His managerial experience coupled with a history of creating and building successful brands, through identity and message development, gives me a unique vision of how these components should fit together for the whole to fully succeed.'
+            },
+            {
+                name: 'Demitri Hopkins',
+                nickname: 'Demitri',
+                position: '',
+                image: demitri,
+                nationalities: ['us'],
+                social: {
+                    linkedIn: {
+                        url: 'https://www.linkedin.com/in/demitri-hopkins-54260a106',
+                        icon: 'fa-linkedin-in'
+                    }
+                },
+                bio: 'Demitri Hopkins has been an advocate of fusion energy with a vision to power the world almost his entire life. He co-founded AGNI Energy Inc. which is developing a modular fusion reactor design combining the stability of magnetic containment with beam to target inertial fusion. He is also working with Stardust389 on its various projects on nanotechnology and fusion.'
+            },
+            {
+                name: 'Denis Lucindo',
+                nickname: 'Denis',
+                position: '',
+                image: denis,
+                nationalities: ['ph'],
+                social: {
+                    linkedIn: {
+                        url: 'https://www.linkedin.com/in/denis-lucindo-649a117',
+                        icon: 'fa-linkedin-in'
+                    }
+                },
+                bio: 'Denis Lucindo has extensive experience in Mergers, Acquisitions, and Business Development, having spent several years as VP for Business Development for Metro Pacific Investments Corp. There, he led several and participated in several acquisitions and bids, including that for Maynilad Inc., which is the largest water distribution company with concessions covering half of Metro Manila and neighboring provinces. He has also held the role of VP for Investor Relations with Metro Pacific and First Pacific, and Chief Strategist for the Ramcar Food Group. Entrepreneurship is a key advocacy for Denis, stemming from his early days as founding Publisher and then Editor-in-Chief of the Philippine edition of Entrepreneur Magazine.'
+            },
+        ]
+    },
+    {
+        team: [
+
+        ]
+    }
 ];
 
 export default advisors
